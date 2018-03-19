3c20975e3012fc9d38693ac684828359383e61d8
Christian Hidalgo <christianhf.chf@gmail.com>
2018-03-19 18:21:53 +0100

    Añade css para pantallas pequeñas

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -33,19 +33,34 @@ AppAsset::register($this);
             <?= Html::a('CHRISTIANHF', Url::to(['/site/index'])) ?>
         </div>
         <div>
-            <ul>
+            <ul id="menu1">
                 <li><?= Html::a('<span class="glyphicon glyphicon-home" title="Inicio" aria-hidden="true"></span>
-                    Inicio', Url::to(['/site/index']), ['id' => 'opc1']) ?></li>
+                    Inicio', Url::to(['/site/index']), ['id' => 'opc1', 'class' => 'opc']) ?></li>
                 <li><?= Html::a('<span class="glyphicon glyphicon-file" title="Mi CV" aria-hidden="true"></span>
-                    Mi CV', Url::to(['/experiencias/cv']), ['id' => 'opc2']) ?></li>
+                    Mi CV', Url::to(['/experiencias/cv']), ['id' => 'opc2', 'class' => 'opc']) ?></li>
                 <li><?= Html::a('<span class="glyphicon glyphicon-send" title="Contacto" aria-hidden="true"></span>
-                    Contacto', Url::to(['/site/contact']), ['id' => 'opc3']) ?></li>
+                    Contacto', Url::to(['/site/contact']), ['id' => 'opc3', 'class' => 'opc']) ?></li>
                 <?php if (Yii::$app->user->isGuest) { ?>
                     <li><?= Html::a('Login', Url::to(['/site/login']), ['id' => 'opc4']) ?></li>
                 <?php } else { ?>
                     <li><?= Html::a('<span class="glyphicon glyphicon-folder-open" title="Experiencias" aria-hidden="true"></span>
-                        Experiencias', Url::to(['/experiencias/index']), ['id' => 'opc5']) ?></li>
+                        Experiencias', Url::to(['/experiencias/index']), ['id' => 'opc5', 'class' => 'opc']) ?></li>
                     <li><a href="<?= Url::to(['/site/logout']) ?>" data-method="POST">Logout (<?= Yii::$app->user->identity->nombre ?>)</a></li>
+                <?php } ?>
+            </ul>
+            <ul id="menu2">
+                <li><?= Html::a('<span class="glyphicon glyphicon-home" title="Inicio" aria-hidden="true"></span>
+                    ', Url::to(['/site/index']), ['id' => 'opc1', 'class' => 'opc']) ?></li>
+                <li><?= Html::a('<span class="glyphicon glyphicon-file" title="Mi CV" aria-hidden="true"></span>
+                    ', Url::to(['/experiencias/cv']), ['id' => 'opc2', 'class' => 'opc']) ?></li>
+                <li><?= Html::a('<span class="glyphicon glyphicon-send" title="Contacto" aria-hidden="true"></span>
+                    ', Url::to(['/site/contact']), ['id' => 'opc3', 'class' => 'opc']) ?></li>
+                <?php if (Yii::$app->user->isGuest) { ?>
+                    <li><?= Html::a('Login', Url::to(['/site/login']), ['id' => 'opc4']) ?></li>
+                <?php } else { ?>
+                    <li><?= Html::a('<span class="glyphicon glyphicon-folder-open" title="Experiencias" aria-hidden="true"></span>
+                        ', Url::to(['/experiencias/index']), ['id' => 'opc5', 'class' => 'opc']) ?></li>
+                    <li><a href="<?= Url::to(['/site/logout']) ?>" data-method="POST"><span class="glyphicon glyphicon-log-out" title="Logout" aria-hidden="true"></span></a></li>
                 <?php } ?>
             </ul>
         </div>
@@ -62,9 +77,14 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; ChristianHF <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
+        <div class="pull-left">
+            <p>&copy; ChristianHF <?= date('Y') ?></p>
+            <p>Sanlúcar de Barrameda (Cádiz)</p>
+        </div>
+        <div class="pull-right">
+            <p>Email: <span>christianhf.chf@gmail.com</span></p>
+            <p>Teléfono: <span>671346980</span></p>
+        </div>
     </div>
 </footer>
 
