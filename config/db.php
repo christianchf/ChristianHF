8fc61b1757f1d3a8d3c6061d3e25b25982aa5099
Christian Hidalgo <christianhf.chf@gmail.com>
2018-03-19 19:56:16 +0100

    Preparado para Heroku

--- config/db.php
+++ config/db.php
@@ -1,14 +1,24 @@
 <?php
-
+$url = getenv('DATABASE_URL');
+if ($url !== false) {
+    $matches = parse_url($url);
+    $host = $matches['host'];
+    $port = $matches['port'];
+    $dbname = substr($matches['path'], 1);
+    $username = $matches['user'];
+    $password = $matches['pass'];
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => "pgsql:host=$host;port=$port;dbname=$dbname",
+        'username' => $username,
+        'password' => $password,
+        'charset' => 'utf8',
+    ];
+}
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'pgsql:host=localhost;dbname=christianhf',
     'username' => 'christianhf',
     'password' => 'christianhf',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
